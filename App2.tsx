6fa89a3f2d161864400747a7e0911992606137a5
hasan4adnan <99694773+hasan4adnan@users.noreply.github.com>
2025-12-15 20:30:22 +0300

    Create tsx
    Create tsx

---
+++ App2.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+export default function App2() {
+  return (
+    <div className="mini-card">
+      <h2>Mini Kart 2</h2>
+      <p>Bu, ikinci bir modern kart örneğidir.</p>
+      <button>Daha Fazla</button>
+    </div>
+  );
+}
